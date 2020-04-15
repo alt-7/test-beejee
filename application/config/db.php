b5b2a63a9d692ae6ca644e26afba5de1cc2b1ca0
Altynbek <altynbek1904@gmail.com>
2020-04-15 17:04:32 +0600

    realease v3

--- application/config/db.php
+++ application/config/db.php
@@ -1,13 +1,8 @@
 <?php
 
 return [
-    //'host' => 'mysql.zzz.com.ua',
-    //'name' => 'altosh',
-    //'user' => 'taskbeejee',
-    //'password' => 'ESIPewp34KSa',
-
-	'host' => 'localhost',
-	'name' => 'beejee',
+    'host' => 'localhost',
+	'name' => '',
 	'user' => 'root',
 	'password' => '',
 ];
